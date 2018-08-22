43fa6ae2e34105f90af7db6959c91b154c15dc68
Andrea Cardini <andrea.cardini4@gmail.com>
2018-08-22 18:58:51 +0200

    Updated plotting macro with variables

--- DMu_plotter.C
+++ DMu_plotter.C
@@ -3,19 +3,27 @@
 #include <map>
 #include <iomanip>
 #include "TPad.h"
+#include "TCanvas.h"
+#include "TGraph.h"
+#include "TMultiGraph.h"
+#include "TH1.h"
 #include "TROOT.h"
 #include "TColor.h"
 #include "TMath.h"
 #include "TPaveLabel.h"
+#include "TStyle.h"
 
 
 void DMu_plotter(string type="MB"){
 	gROOT->SetBatch();
 	vector<string> structures { "BPIX", "FPIX", "BPIX_y", "FPIX_y", "TIB", "TOB" };
+	vector<string> variables { "mu", "dmu", "sigma", "sigma_mu" };
+	vector<string> YaxisNames { "#mu", "#Delta#mu", "#sigma", "#sigma_{#mu}" };
 	vector<string> geometries {"GT" ,"SG" ,"MPpixLBL" ,"MPpixLBLstr" ,"MP2pixLBLHGIOV" ,"MP2pixHMSHGIOV" ,"MP2pixHMSstrHGIOV" ,"HipPypix"};
-	vector<Color_t> colours { kBlack, kRed, kBlue, kGreen+2, kOrange, kCyan+2, kYellow+2, kViolet}; // TODO: correct the colours
+	vector<Color_t> colours { kBlack, kRed, kBlue, kGreen+2, kOrange, kCyan+2, kYellow+2, kViolet}; 
 	int i=0;
-	for (string structure: structures) {
+	for (string variable: variables) {
+	  for (string structure: structures) {
 	        double max=-999;
 		double min=+999;
 		string canvasname = structure;
@@ -24,7 +32,7 @@ void DMu_plotter(string type="MB"){
 		vector<Color_t>::iterator colour = colours.begin();
 		TMultiGraph *mg = new TMultiGraph(structure.c_str(),structure.c_str());
 		for (string geometry: geometries) {
-  			string filename = "dMuDMR_"+type+"/dmu_"+type+"_" + structure + "_" + geometry + ".dat";
+  			string filename = "dMuDMR_"+type+"/"+variable+"_"+type+"_" + structure + "_" + geometry + ".dat";
  			cout << filename << '\t' << *colour << endl;
 			TGraph * g = new TGraph(filename.c_str());
 			if(g==NULL)continue;
@@ -46,7 +54,9 @@ void DMu_plotter(string type="MB"){
 
 		mg->SetMaximum(max+range*0.1);
 		mg->SetMinimum(min-range*0.3);
-		mg->GetYaxis()->SetTitle("#Delta#mu");
+		char* Ytitle= (char *)YaxisNames.at(i).c_str();
+		mg->GetYaxis()->SetTitle(Ytitle);
+
 		mg->GetXaxis()->SetTitle("run number");
 		mg->GetYaxis()->SetTitleOffset(.8);
 		mg->GetYaxis()->SetTitleSize(.05);
@@ -56,19 +66,21 @@ void DMu_plotter(string type="MB"){
 		c->BuildLegend();
 		cout << "pad max " << gPad->GetUymax() << " pad min " << gPad->GetUymin() << endl;
 		cout << "graph max " << max << " graph min " << min << endl;
-		char* typetitle;
+		char* typetitle=(char *)"";
 		if(type=="MB")typetitle=(char *)"Minimum Bias";
 		if(type=="SM")typetitle=(char *)"Single Muon";
 		TPaveLabel *title = new TPaveLabel(.11,.91,.89,0.99,typetitle,"brNDC");
 		c->cd();
 		title->Draw();
 		c->Update();
-		string pngfile=type+"_"+structure+ ".png";
-		string pdffile=type+"_"+structure+ ".pdf";
-		string epsfile=type+"_"+structure+ ".eps";
+		string pngfile=type+"_"+variable+"_"+structure+ ".png";
+		string pdffile=type+"_"+variable+"_"+structure+ ".pdf";
+		string epsfile=type+"_"+variable+"_"+structure+ ".eps";
 		c->Print(pdffile.c_str());
 		c->Print(pngfile.c_str());
 		c->Print(epsfile.c_str());
-		++i;
+	  }
+	  ++i;
+
 	}
 }
