89c2d5fceae20874de61d27f6f9bac799c208394
Andrea Cardini <andrea.cardini4@gmail.com>
2018-12-17 17:50:20 +0100

    Updated with more comments, the plotting macro has been temporary merged with the tool to create the DMR trends to simplify the execution of the macro

--- DMRs/DMR_MB_315690_v11/DMRtrends.C
+++ DMRs/DMR_MB_315690_v11/DMRtrends.C
@@ -87,6 +87,10 @@ struct Point {
             else return SFactor*hypot(sigmaplus,sigmaminus); };
 };
 
+/*! \function getName
+ *  \brief Function used to get a string containing information on the high level structure, the layer/disc and the geometry.
+ */
+
 TString getName (TString structure, int layer, TString geometry){
     geometry.ReplaceAll(" ","_");
     TString name=geometry+"_"+structure;
@@ -101,9 +105,14 @@ TString getName (TString structure, int layer, TString geometry){
 
 
 void scalebylumi(TGraphErrors *g, double min=0., string scalefile="/afs/cern.ch/work/h/hpeterse/public/lumiPerRun80.csv"); 
-//old /afs/cern.ch/work/h/hpeterse/public/lumiPerRun80.csv
-//new /afs/cern.ch/work/h/hpeterse/public/lumiPerRun80.csv
 double scalerunbylumi(int run, double min=0., string scalefile="/afs/cern.ch/work/h/hpeterse/public/lumiPerRun80.csv");
+void PlotDMRTrends(string label="_v11", string type="MB", const vector<int> IOVfirstrun={315257, 315488, 315489, 315506, 315640, 315689, 315690, 315713, 315790, 315800, 315973, 316058, 316060, 316082, 316187, 316200, 316216, 316218, 316239, 316271, 316361, 316363, 316378, 316456, 316470, 316505, 316559, 316569, 316665, 316758, 317080, 317182, 317212, 317295, 317339, 317382, 317438, 317527, 317661, 318228, 318712, 319337, 319460, 320377, 320569, 320688, 320712, 320809, 320821, 320823, 320838, 320853, 320856, 320917, 320933, 320936, 320980, 321004, 321009, 321051, 321067, 321119}, vector<string> geometries={"MP pix LBL","PIX HLS+ML STR fix","GT","SG"}, vector<Color_t> colours={kGreen, kBlue, kBlack, kRed});
+
+/*! \class Geometry
+ *  \brief Class Geometry
+ *         Contains vector for fit parameters (mean, sigma, etc.) obtained from multiple IOVs
+ *         See Structure Point for description of the parameters.
+ */
 
 class Geometry {
     public:
@@ -138,6 +147,7 @@ class Geometry {
         // };
 };
 
+/// DEPRECATED
 //struct Layer {
 //    map<string,Geometry> geometries;
 //};
@@ -147,15 +157,21 @@ class Geometry {
 //    map<string,Geometry> geometries;
 //};
 
+
+/*! \function DMRtrends()
+ *  \brief    Function DMRtrends()
+ *            Used to create a file where DMR trends are stored in the form of TGraph.
+ */
+
+
 void DMRtrends(string label="_v11", string myValidation="/afs/cern.ch/cms/CAF/CMSALCA/ALCA_TRACKERALIGN/data/commonValidation/results/acardini/DMRs/", vector<string> geometries={"MP pix LBL","PIX HLS+ML STR fix","GT","SG"}, string type="MB"){
     gROOT->SetBatch();
-    std::cout << __cplusplus << std::endl;
     vector<int>RunNumbers;
     vector<TString> filenames;
     TRegexp regexp("[0-9][0-9][0-9][0-9][0-9][0-9]");
     for (const auto & entry : fs::recursive_directory_iterator(myValidation)){
       if (entry.path().string().find("ExtendedOfflineValidation_Images/OfflineValidationSummary.root")!=std::string::npos){
-	std::cout << entry.path().string() << std::endl;
+	//std::cout << entry.path().string() << std::endl;
 	if(fs::is_empty(entry.path())) cout << "ERROR: Empty file " << entry.path() << endl;
 	else{
 	  TString filename(entry.path().string());
@@ -200,8 +216,8 @@ void DMRtrends(string label="_v11", string myValidation="/afs/cern.ch/cms/CAF/CM
         for (TString& structure: structures) {
             TString structname = structure;
             structname.ReplaceAll("_y", "");
-            int layersnumber=nlayers.at(structname);
-            for (int layer=0; layer<=layersnumber;layer++){
+            size_t layersnumber=nlayers.at(structname);
+            for (size_t layer=0; layer<=layersnumber;layer++){
                 for (string geometry: geometries) {
 
                     TString name = getName(structure, layer, geometry);
@@ -238,8 +254,8 @@ void DMRtrends(string label="_v11", string myValidation="/afs/cern.ch/cms/CAF/CM
     for (TString& structure: structures) {
         TString structname = structure;
         structname.ReplaceAll("_y", "");
-        int layersnumber=nlayers.at(structname);
-        for (int layer=0; layer<=layersnumber;layer++){
+        size_t layersnumber=nlayers.at(structname);
+        for (size_t layer=0; layer<=layersnumber;layer++){
             for (string geometry : geometries) {
                 TString name = getName(structure, layer, geometry);
                 Geometry geom = mappoints[make_pair(make_pair(structure,layer),geometry)];
@@ -274,7 +290,7 @@ void DMRtrends(string label="_v11", string myValidation="/afs/cern.ch/cms/CAF/CM
     }
     fout->Close();
 
-
+    PlotDMRTrends(label, type, RunNumbers, geometries);
 }
 
 
@@ -348,6 +364,123 @@ void scalebylumi(TGraphErrors *g, double min, string scalefile){
     cout << "total lumi: " << lumi << endl;
     g->GetHistogram()->Delete(); 
     g->SetHistogram(0); 
+}
+/*! \function PlotDMRTrends()
+ *  \brief    Function PlotDMRTrends()
+ *            Used to plot the DMR trends.
+ */
+
+void PlotDMRTrends(string label, string type, const vector<int> IOVfirstrun, vector<string> geometries, vector<Color_t> colours){
+
+    vector<int> pixelupdateruns {314881, 316758, 317527, 318228, 320377};
+
+    vector<TString> structures { "BPIX", "BPIX_y", "FPIX", "FPIX_y", "TIB", "TID", "TOB", "TEC"};
+
+    const map<TString,int> nlayers{ {"BPIX", 4}, {"FPIX", 3}, {"TIB", 4}, {"TID", 3}, {"TOB", 6}, {"TEC", 9} };
+
+
+    //	vector<string> geometries {"GT", "PIXHLS+MLSTRfix"};
+    //	vector<Color_t> colours { kBlack, kRed}; 
+    TString filename="/afs/cern.ch/cms/CAF/CMSALCA/ALCA_TRACKERALIGN/data/commonValidation/results/acardini/DMRs/DMRtrends_"+type+".root";
+    TFile *in= new TFile(filename);
+    for (TString& structure: structures) {
+        TString structname = structure;
+        structname.ReplaceAll("_y", "");
+        int layersnumber=nlayers.at(structname);
+        for (int layer=0; layer<=layersnumber;layer++){
+            vector<TString> variables {"mu", "sigma", "muplus", "sigmaplus", "muminus", "sigmaminus", "deltamu", "sigmadeltamu", "musigma", "muplussigmaplus", "muminussigmaminus", "deltamusigmadeltamu"};
+            vector<string> YaxisNames { "#mu [#mum]", "#sigma_{#mu} [#mum]", "#mu outward [#mum]", "#sigma_{#mu outward} [#mum]", "#mu inward [#mum]", "#sigma_{#mu inward} [#mum]", "#Delta#mu [#mum]", "#sigma_{#Delta#mu} [#mum]", "#mu [#mum]", "#mu outward [#mum]", "#mu inward [#mum]", "#Delta#mu [#mum]",}; 
+            for(size_t i=0; i < variables.size(); i++){
+                TString variable= variables.at(i);
+                double max=-999;
+                double min=+999;
+                TCanvas * c = new TCanvas;
+                vector<Color_t>::iterator colour = colours.begin();
+
+                TMultiGraph *mg = new TMultiGraph(structure,structure);
+
+                for (string geometry: geometries) {
+                    TString name = getName(structure, layer, geometry);
+                    TGraphErrors *g = (TGraphErrors*) in->Get(name+"_"+variables.at(i));
+                    /*if (i >=8) {
+                      g->Delete();
+                      TGraphErrors *g = (TGraphErrors*) in->Get(name+"_"+variables.at(i));
+                      }*/ 
+                    g->SetLineColor(*colour);
+                    g->SetMarkerColor(*colour);
+                    g->SetMarkerStyle(20);
+                    if(i>=8){
+                        g->SetLineWidth(2);
+                        g->SetFillColor(*colour);
+                        g->SetFillStyle(3350+i-8);
+                    }
+                    if(i<8) mg->Add(g,"PL");
+                    else mg->Add(g,"3L");
+                    if(g->GetHistogram()->GetMaximum() > max) max = g->GetHistogram()->GetMaximum();
+                    if(g->GetHistogram()->GetMinimum() < min) min = g->GetHistogram()->GetMinimum();
+                    ++colour;
+
+                }
+                if(i<8) mg->Draw("a");
+                else mg->Draw("a3");
+                max=0.4;
+                min=-0.4;
+                double range=max-min;
+                if(((variable=="sigma"||variable=="sigmaplus"||variable=="sigmaminus"||variable=="sigmadeltamu")&&range>=2)){
+                    mg->SetMaximum(0.5);
+                    mg->SetMinimum(-0.5);
+                }else{
+                    mg->SetMaximum(max+range*0.1);
+                    mg->SetMinimum(min-range*0.3);
+                }
+                char* Ytitle= (char *)YaxisNames.at(i).c_str();
+                mg->GetYaxis()->SetTitle(Ytitle);
+
+                mg->GetXaxis()->SetTitle("IOV number");
+                mg->GetYaxis()->SetTitleOffset(.8);
+                mg->GetYaxis()->SetTitleSize(.05);
+                mg->GetXaxis()->SetTitleSize(.04);
+                gStyle->SetOptTitle(0); // TODO
+                c->SetLeftMargin(0.11);
+                char* typetitle=(char *)"";
+                if(type=="MB")typetitle=(char *)"Minimum Bias";
+                if(type=="SM")typetitle=(char *)"Single Muon";
+
+                c->Update();
+
+
+                TLegend *legend = c->BuildLegend();
+                legend->SetHeader(typetitle);
+                TLegendEntry *header = (TLegendEntry*)legend->GetListOfPrimitives()->First();
+                header->SetTextSize(.04);
+                legend->SetNColumns(2);
+                //legend->SetTextSize(0.05);
+                TString structtitle = "structure: " +structure;
+                if(layer!=0){
+                    if(structure=="TID"||structure=="TEC")structtitle+="_disc";
+                    else structtitle+="_layer";
+                    structtitle+=layer;
+                }
+                legend->AddEntry((TObject*)0,structtitle.Data(),"h");
+                TLegendEntry *str = (TLegendEntry*)legend->GetListOfPrimitives()->Last();
+                str->SetTextSize(.03);
+                //cout << "pad max " << gPad->GetUymax() << " pad min " << gPad->GetUymin() << endl;
+                //cout << "graph max " << max << " graph min " << min << endl;
+                legend->Draw();
+                c->Update();
+                TString structandlayer = getName(structure,layer,"");
+		///TO DO: select output directory for the printing the DMR trends
+                TString printfile="/afs/cern.ch/cms/CAF/CMSALCA/ALCA_TRACKERALIGN/data/commonValidation/alignmentObjects/acardini/DMRsTrends/V11-"+type+"_"+variable+structandlayer;
+                c->SaveAs(printfile+".pdf");
+                c->SaveAs(printfile+".eps");
+                c->SaveAs(printfile+".png");
+
+            }
+
+        }
+
+    }
+    in->Close();
 }
 
 int main () { DMRtrends(); return 0; }
